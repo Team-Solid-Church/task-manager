8ee8041bb6a523fbf5fee30c5ccbb7c246ce3535
Jason Mize <jasonemize@gmail.com>
2016-09-07 16:32:36 -0400

    sub menus not drawing

--- display.py
+++ display.py
@@ -1,28 +1,29 @@
 import os
 
+MENU = {
+    "1" : "1 = Add Task",
+    "2" : "2 = Remove Task",
+    "3" : "3 = Mark Complete",
+    "4" : "4 = List Completed Tasks",
+    "5" : "5 = List Unfinished Tasks",
+    "Q" : "Q = Exit",
+}    
+
 class Display():
     """What the user will actually see on the screen."""
 
     def __init__(self, task_list):
         self.display_info = ""
         self.task_list = task_list
-
-    MENU = {
-        "1" : "1 = Add Task",
-        "2" : "2 = Remove Task",
-        "3" : "3 = Mark Complete",
-        "4" : "4 = List Completed Tasks",
-        "5" : "5 = List Unfinished Tasks",
-        "Q" : "Q = Exit",
-    }    
-
-    def title (self):
-        print("\tTASK MANAGER")        
+        self.current_menu = MENU
 
     def menu(self):
         print("\n")
-        for value in sorted(self.MENU.values()):
+        for value in sorted(self.current_menu.values()):
             print ("\t" + value)
+
+    def title (self):
+        print("\tTASK MANAGER")        
 
     #information sent back to user from the input_handler        
     def handler_output (self):
@@ -34,8 +35,8 @@ class Display():
     def build_display(self):
         # import pdb; pdb.set_trace()    	
         
-        os.system('clear')
-        # print("*" * 10)
+        # os.system('clear')
+        print("*" * 10)
 
         self.title()
 
